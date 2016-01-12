fcee9ab9788b12c07302edd10210e921bf3ac02b
anitayen423 <anitayen423@gmail.com>
2016-01-11 23:35:39 -0800

    Create Cow.java

---
+++ Cow.java
@@ -0,0 +1,148 @@
+
+import java.util.Objects;
+import java.util.concurrent.ThreadLocalRandom;
+
+/*
+ * Fields
+ * String for name
+ * int for level of hungriness
+ * int for age
+ * int sicknessLevel
+
+ * Behavior:
+ * From 6AM to 6PM, a cow traverses one square in a random direction each hour. 
+ * If the chosen square is occupied by something that is not grass or the cow 
+ * would go off the grid, do nothing for that hour.
+ * At any other time, the cow is asleep, and does nothing.
+ * If the cow attempts to move to a square filled with grass, the grass is removed, 
+ * the cow’s hungriness decreases by the amount of grass, and the cow moves to the square.
+ * Every hour, the cow's hungriness level increases by 2 and age by 1.
+ * If hungriness level reaches 100 or age reaches 90001, the cow dies and is removed from the farm.
+ * Cow has a (0.0001*age*sicknessLevel)% chance of dying each hour. Dead cows are removed from the farm.
+ * 
+ * @author Anita Yen
+ */
+public class Cow extends FarmObject {
+
+    private String name;
+    private int hungriness, sicknessLevel, age;
+
+    public Cow(String name, int hungriness, int sicknessLevel, int age, int x, int y) {
+        super(x, y);
+        this.name = name;
+        this.hungriness = hungriness;
+        this.sicknessLevel = sicknessLevel;
+        this.age = age;
+    }
+
+    public Cow(int x, int y) {
+        super(x, y);
+        name = "Empty";
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getHungriness() {
+        return hungriness;
+    }
+
+    public void setHungriness(int hungriness) {
+        this.hungriness = hungriness;
+    }
+
+    public int getSicknessLevel() {
+        return sicknessLevel;
+    }
+
+    public void setSicknessLevel(int sicknessLevel) {
+        this.sicknessLevel = sicknessLevel;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getNewX() {
+        return newX;
+    }
+
+    public int getNewY() {
+        return newY;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 23 * hash + Objects.hashCode(this.name);
+        hash = 23 * hash + this.hungriness;
+        hash = 23 * hash + this.sicknessLevel;
+        hash = 23 * hash + this.age;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Cow other = (Cow) obj;
+        return this.name.equals(other.getName());
+    }
+
+    /**
+     *
+     * @param hour
+     * @return true means cow need to die
+     */
+    @Override
+    public boolean doStuffInAHour(int hour) {
+        hungriness += 2;
+        age += 1;
+        return hungriness >= 100 || age >= 90001 || dieOfSickness();
+    }
+
+    /**
+     *
+     * @return true if cow need to die because of random chance
+     */
+    public boolean dieOfSickness() {
+        int p = (int) (0.01 * age * sicknessLevel);
+        int randomNumber = ThreadLocalRandom.current().nextInt(0, 101);
+        return randomNumber <= p;
+    }
+
+    /**
+     * update cow new location
+     */
+    public void UpdateToNewLocation() {
+        this.setX(newX);
+        this.setY(newY);
+    }
+
+    public boolean awake() {
+        return WorldObject.currentHour >= 6 && WorldObject.currentHour < 18;
+    }
+
+    @Override
+    public String toString() {
+        return "Cow{" + super.toString() + ", name=" + name + ", hungriness=" + hungriness + ", sicknessLevel=" + sicknessLevel + ", age=" + age + '}';
+    }
+
+    @Override
+    public void printObjectID() {
+        System.out.print(COW);
+    }
+}
